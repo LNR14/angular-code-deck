566db2ac0453b9c4227c4647628d02de2e3f61b1
Lakshmi narayana <lnrabc@gmail.com>
2024-08-26 09:21:06 +0530

    products display complete

--- storeFront/src/app/components/product/product.component.ts
+++ storeFront/src/app/components/product/product.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../types';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [RatingModule, FormsModule],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
-
+  @Input() product!: Product;
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.product);
+  }
 }
